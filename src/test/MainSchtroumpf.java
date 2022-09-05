65bc56c34987f83c695ac8aa13ef077e3dd45665
Furiza31 <hugo.wendjaneh@gmail.com>
2022-09-05 16:31:24 +0000

    On ajoute les schtroumpfs

---
+++ src/test/MainSchtroumpf.java
@@ -0,0 +1,32 @@
+package test;
+import lesschtroumpfs.Schtroumpf;
+
+
+public class MainSchtroumpf {
+
+	public static void main (String[] argv) {
+		Schtroumpf grognon;
+		
+		grognon = new Schtroumpf("grognon", 135);
+		
+		grognon.sePresenter();
+		grognon.chanter();
+		
+		grognon.allerTravailler();
+		
+		grognon.sePresenter();
+		grognon.chanter();
+		
+		grognon.manger(10);
+		
+		grognon.sePresenter();
+		grognon.chanter();
+		
+		grognon.anniversaire();
+		
+		grognon.sePresenter();
+		grognon.chanter();
+		
+	}
+
+}
